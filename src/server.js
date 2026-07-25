4a108e70d350b7665f4c4240582f611a62446431
nebiyumathewos01-cell <nebiyumathewos01@gmail.com>
2026-07-25 23:53:39 +0300

    Fix CORS for production

--- src/server.js
+++ src/server.js
@@ -8,14 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    process.env.FRONTEND_URL,
-  ].filter(Boolean),
-  credentials: true,
-}));
+app.use(cors());;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
